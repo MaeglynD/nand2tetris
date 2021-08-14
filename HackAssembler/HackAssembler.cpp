65481046b04fd7ff3fac7ecd89ee5c575648eeca
MaeglynD <maeglyn.davies@gmail.com>
2021-08-14 11:08:47 +0100

    progress on assembler

--- HackAssembler/HackAssembler.cpp
+++ HackAssembler/HackAssembler.cpp
@@ -1,33 +1,57 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Parser {
-	ifstream fileContents;
-	string currentCommand;
-
 	public:
-		Parser(string fileName){
+		vector<string> commands;
+		int currentCommandIndex = -1;
+
+		Parser(string fileName) {
+			string line;
+			ifstream fileContents;
+			
 			fileContents.open(fileName);
+			
+			while(getline(fileContents, line)) {
+				line.erase(remove(line.begin(), line.end(), ' '), line.end());
+
+				// If theres anything in the line, and its not a comment
+				if (line != "" && line.substr(0, 2) != "//") {
+					commands.push_back(line);
+				}
+			}
+		}
+
+		bool hasMoreCommands() {
+			return currentCommandIndex < commands.size();
 		}
 
 		void advance() {
-			getline(fileContents, currentCommand);
+			currentCommandIndex++;
 		}
 
-		bool hasMoreCommands(){
-			return fileContents.peek() != -1;
-		}
+		string commandType() {
+			char firstLetter = commands.at(currentCommandIndex)[0];
 
-		string commandType(){
-			currentCommand.erase(remove(currentCommand.begin(), currentCommand.end(), ' '), currentCommand.end());
-			
-			if(currentCommand[0] == '@'){
-				// A command
-			}else{
-				// 
+			if (firstLetter == '@') {
+				return "A_COMMAND";
 			}
+
+			if (firstLetter == '(') {
+				return "L_COMMAND";
+			}
+
+			return "C_COMMAND";
+		}
+
+		string symbol() {
+			string currentCommand = commands.at(currentCommandIndex);
+			bool isLCommand = currentCommand[currentCommand.size() - 1] == ')';
+
+			return currentCommand.substr(1, - (isLCommand ? 2 : 1));
 		}
 };
 
