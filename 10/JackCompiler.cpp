e96c039c616798e02657971363945922775c690d
MaeglynD <maeglyn.davies@gmail.com>
2021-09-25 14:57:03 +0100

    added foundation to jack compiler

---
+++ 10/JackCompiler.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <experimental/filesystem>
+#include <regex>
+#include <cctype>
+using namespace std;
+namespace fs = std::experimental::filesystem;
+
+class CompilitationEngine {
+
+	CompilitationEngine() {
+		// 
+	}
+
+	void compileClass() {
+		// 
+	}
+
+	void compileClassVarDec() {
+		// 
+	}
+
+	void compileSubroutine(){ 
+
+	}
+
+	void compileParameterList() {
+		// 
+	}
+
+	void compileVarDev() {
+		// 
+	}
+
+	void compileStatements() {
+		// 
+	}
+
+	void compileDo() {
+		// 
+	}
+
+	void compileLet(){
+		// 
+	}
+
+	void compileWhile() {
+		// 
+	}
+
+	void compileReturn() {
+		// 
+	}
+
+	void compileIf() {
+		// 
+	}
+
+	void compileExpression() {
+		// 
+	}
+
+	void compileTerm() {
+		// 
+	}
+
+	void compileExpressionList() {
+		// 
+	}
+};
+
+class JackTokenizer {
+
+	JackTokenizer() {
+		// 
+	}
+
+	bool hasMoreTokens() {
+		return false;
+	}
+	
+	void advance() {
+		// 
+	}
+
+	string tokenType() {
+		return "";
+	}
+
+	string keyWord() {
+		return "";
+	}
+
+	char symbol() {
+		return 'A';
+	}
+
+	string identifier() {
+		return "";
+	}
+
+	int intVal() {
+		return 0;
+	}
+
+	string stringVal() {
+		return "";
+	}
+
+};
+
+int main(){
+	string userInput;
+	cin >> userInput;
+
+	return 0;
+}
